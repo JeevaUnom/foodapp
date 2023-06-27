d7152111f9296fd50b39cf1f8ea2b283868bee4f
Jeeva <136064402+JeevaUnom@users.noreply.github.com>
2023-06-27 06:23:47 +0000

    Update 4 files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ export default function Navbar(){
     return (
         <>
             <div className="navbar container">
-                <Link to="/" className="logo">F<span>oo</span>dCourt</Link>
+                <Link to="/" className="logo">F<span class="emoji">&#128523;&#128523;</span>dCourt</Link>
                 <div className="nav-links">
                     { links.map(link => (
                         <Link className={location.pathname === link.path ? "active" : ""} to={link.path} key={link.name}>{link.name}</Link>
